3fcc5398c9d350d9e4f270ce694e168104619f1e
Dima Ramaniuk <kanumowa@gmail.com>
2021-10-18 13:16:44 +0200

    complete quiz2

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,9 +33,18 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        match fruit {
+            Fruit::Apple | Fruit::Mango | Fruit::Lychee => {},
+            Fruit::Banana => {
+                basket.insert(Fruit::Banana, 4);
+            },
+            Fruit::Lychee => {
+                basket.insert(Fruit::Lychee, 4);
+            },
+            Fruit::Pineapple => {
+                basket.insert(Fruit::Pineapple, 4);
+            },
+        }
     }
 }
 
--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,16 +1,33 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 impl Message {
     fn call(&self) {
         println!("{:?}", &self);
+        match self {
+            Message::Quit => {
+                println!("The Quit variant has no data to destructure.")
+            }
+            Message::Move { x, y } => {
+                println!(
+                    "Move in the x direction {} and in the y direction {}",
+                    x, y
+                );
+            }
+            Message::Echo(text) => println!("{:?}", &self),
+            Message::ChangeColor(r, g, b) => println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r, g, b
+            ),
+        }
     }
 }
 
